a0778a31559ae304a19a4a6d79a67b2fd9c2a334
sandymaramma <edesandraoge@gmail.com>
2023-11-05 23:00:22 +0100

    update

--- my_project/forms.py
+++ my_project/forms.py
@@ -7,7 +7,7 @@ class SignupForm(FlaskForm):
     username = StringField('Username', validators = [DataRequired(), Length(min=2, max=20)])
     email = StringField('Email Address', validators = [DataRequired(), Email()])
     password = PasswordField('Password', validators = [DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqaulTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 
